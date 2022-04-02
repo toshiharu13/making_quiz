04f6d754273a40661b8adb0354e0ebb7fca55bdf
elf <elorian@yandex.ru>
2022-04-02 20:15:44 +0300

    Add keyboard to VK bot

--- main_vk.py
+++ main_vk.py
@@ -1,25 +1,34 @@
-import random
-from environs import Env
-import vk_api as vk
+import vk_api
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
+from environs import Env
+import random
 
 
-def echo(event, vk_api):
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1, 1000)
-    )
-
-
-if __name__ == "__main__":
+def main():
     env = Env()
     env.read_env()
 
-    vk_session = vk.VkApi(token=env.str('VK_API_KEY'))
-    vk_api = vk_session.get_api()
+    vk_session = vk_api.VkApi(token=env.str('VK_API_KEY'))
+    vk = vk_session.get_api()
+
+    keyboard = VkKeyboard(one_time=True)
+
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+
+    keyboard.add_line()  # Переход на вторую строку
+    keyboard.add_button('Мой счёт', color=VkKeyboardColor.PRIMARY)
+
+
+    vk.messages.send(
+        peer_id=env.str('USER_ID'),
+        random_id=random.randint(1, 1000),
+        keyboard=keyboard.get_keyboard(),
+        message=' Да прибудет с тобой сила!'
+    )
+
     longpoll = VkLongPoll(vk_session)
 
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+if __name__ == '__main__':
+    main()
